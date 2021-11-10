58f77c15a348b5a5e4c9577eefd7e80ba8fac752
Calebbii <biicaleb316@gmail.com>
2021-11-11 00:52:32 +0300

    adds start.sh

--- config.py
+++ config.py
@@ -10,9 +10,7 @@ class Config:
     SECRET_KEY = os.environ.get("SECRET_KEY")
 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    MAIL_SERVER = 'smtp.googlemail.com'
-    MAIL_PORT = 587
-    MAIL_USE_TLS = True
+    
     @staticmethod
     def init_app(app):
         pass
